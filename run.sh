5a381f76e0e8a4449cfbf401050e86a447d6b856
Ihar Suvorau <ihar.suvorau@gmail.com>
2023-04-21 17:09:46 +0300

    Fixes shell script

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 if [ -n "$REPORT_OUTPUT_DIR" ]; then
-  timestamp=$(date +%Y%m%d%H%M%S)
-  output_dir="$REPORT_OUTPUT_DIR/$timestamp"
-  mkdir -p "$output_dir"
-
-  echo "Running as master node"
-  locust -f src/locustfile.py \
+    timestamp=$(date +%Y%m%d%H%M%S)
+    output_dir="$REPORT_OUTPUT_DIR/$timestamp"
+    mkdir -p "$output_dir"
+    
+    echo "Running as master node"
+    locust -f src/locustfile.py \
     --host $LOCUST_HOST \
     --users $LOCUST_USERS \
     --spawn-rate $LOCUST_SPAWN_RATE \
@@ -20,8 +20,10 @@ if [ -n "$REPORT_OUTPUT_DIR" ]; then
     --autoquit 1 \
     --master
     $LOCUST_LOAD_PROFILE
-
+    
 else
-  echo "Running as worker node"
-  locust --worker --master-host=$LOCUST_MASTER_NODE_HOST
+    echo "Running as worker node"
+    locust -f src/locustfile.py \
+    --worker \
+    --master-host=$LOCUST_MASTER_NODE_HOST
 fi
