1af67a6a5e5314430fe54f972d6ffc3400869ca3
Ihar Suvorau <ihar.suvorau@gmail.com>
2023-04-21 16:56:43 +0300

    Load profiles added for two payloads

--- src/locustfile.py
+++ src/locustfile.py
@@ -2,50 +2,95 @@ import os
 import random
 from pathlib import Path
 
-from locust import task, FastHttpUser
+from locust import FastHttpUser, constant, task
 from locust.contrib.fasthttp import FastHttpSession
 
 
 class BaseProfile(FastHttpUser):
-    host = os.environ.get('SIMOD_HTTP_URL')
-    endpoint_url = f'{host}/discoveries'
-    assets_dir = Path(__file__).parent.parent / 'assets'
+    host = os.environ.get("SIMOD_HTTP_URL")
+    endpoint_url = f"{host}/discoveries"
+    assets_dir = Path(__file__).parent.parent / "assets"
+    wait_time = constant(1)
 
+
+class ExponentialProfile(BaseProfile):
     def wait_time(self):
         return random.expovariate(1 / 15)
 
 
-class PrepaidTravelCost(BaseProfile):
+class PrepaidTravelCostSingleTask(BaseProfile):
     @task
-    def post1(self):
-        make_post(self.client, self.assets_dir, self.endpoint_url, 'PrepaidTravelCost.xes')
+    def post(self):
+        make_post(
+            self.client, self.assets_dir, self.endpoint_url, "PrepaidTravelCost.xes"
+        )
+
+
+class PrepaidTravelCostExponential(ExponentialProfile):
+    @task
+    def post(self):
+        make_post(
+            self.client, self.assets_dir, self.endpoint_url, "PrepaidTravelCost.xes"
+        )
 
     @task
     def post2(self):
-        make_post(self.client, self.assets_dir, self.endpoint_url, 'PrepaidTravelCost.xes')
+        make_post(
+            self.client, self.assets_dir, self.endpoint_url, "PrepaidTravelCost.xes"
+        )
 
-
-class RequestForPayment(BaseProfile):
     @task
-    def post1(self):
-        make_post(self.client, self.assets_dir, self.endpoint_url, 'RequestForPayment.xes')
+    def post3(self):
+        make_post(
+            self.client, self.assets_dir, self.endpoint_url, "PrepaidTravelCost.xes"
+        )
+
+
+class RequestForPaymentSingleTask(BaseProfile):
+    @task
+    def post(self):
+        make_post(
+            self.client, self.assets_dir, self.endpoint_url, "RequestForPayment.xes"
+        )
+
+
+class RequestForPaymentExponential(ExponentialProfile):
+    @task
+    def post(self):
+        make_post(
+            self.client, self.assets_dir, self.endpoint_url, "RequestForPayment.xes"
+        )
 
     @task
     def post2(self):
-        make_post(self.client, self.assets_dir, self.endpoint_url, 'RequestForPayment.xes')
+        make_post(
+            self.client, self.assets_dir, self.endpoint_url, "RequestForPayment.xes"
+        )
+
+    @task
+    def post3(self):
+        make_post(
+            self.client, self.assets_dir, self.endpoint_url, "RequestForPayment.xes"
+        )
 
 
-def make_post(client: FastHttpSession, assets_dir: Path, endpoint_url: str, event_log_name: str):
-    configuration_path = assets_dir / 'sample.yaml'
+def make_post(
+    client: FastHttpSession, assets_dir: Path, endpoint_url: str, event_log_name: str
+):
+    configuration_path = assets_dir / "sample.yaml"
     event_log_path = assets_dir / event_log_name
 
     data = {
-        'configuration': ('configuration.yaml', configuration_path.open('rb'), 'text/yaml'),
-        'event_log': ('event_log.xes', event_log_path.open('rb'), 'application/xml'),
+        "configuration": (
+            "configuration.yaml",
+            configuration_path.open("rb"),
+            "text/yaml",
+        ),
+        "event_log": ("event_log.xes", event_log_path.open("rb"), "application/xml"),
     }
 
     client.post(
         endpoint_url,
-        headers={'Content-Type': 'multipart/form-data'},
+        headers={"Content-Type": "multipart/form-data"},
         files=data,
     )
